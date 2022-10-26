197a57bdeea0203ec592e2e4f1ee407bfada5349
Oliulla <razasm29@gmail.com>
2022-10-26 23:55:37 +0600

    implement login with email and password

--- src/Contexts/AuthProvider.js
+++ src/Contexts/AuthProvider.js
@@ -1,6 +1,14 @@
 import React, { useEffect } from "react";
 import { createContext } from "react";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import app from "../firebase/firebase.config";
 import { useState } from "react";
 
@@ -8,34 +16,40 @@ export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+    const googleProvider = new GoogleAuthProvider();
+  const [user, setUser] = useState(null);
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
+  const logIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logInWithGoogle = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
   const logOut = () => {
     return signOut(auth);
-  }
-
+  };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-        setUser(currentUser);
-    })
-  
+      setUser(currentUser);
+    });
+
     return () => {
       unsubscribe();
-    }
-  }, [])
-  
+    };
+  }, []);
+
   console.log(user);
 
-  const authInfo = { user, createUser, logOut };
+  const authInfo = { user, createUser, logOut, logIn };
 
   return (
-    <AuthContext.Provider value={authInfo}>
-        {children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
 };
 
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,62 @@
-import React from 'react';
+import React from "react";
+import { useContext } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { AuthContext } from "../../Contexts/AuthProvider";
 
 const Login = () => {
-    return (
-        <div className="bg-dark text-white w-50 mx-auto my-4 px-2 py-2 px-lg-4 py-lg-4 rounded">
-            <div>
-                <h2 className="text-center">Login</h2>
-            </div>
+    const {logIn} = useContext(AuthContext);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+        logIn(email, password)
+            .then((result) => {
+                const user = result.user;
+                console.log(user)
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+
+
+  return (
+    <div className="bg-dark text-white w-50 mx-auto my-4 px-2 py-2 px-lg-4 py-lg-4 rounded">
+      <div>
+        <h2 className="text-center">Login</h2>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control name="email" type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control name="password" type="password" placeholder="Password" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit" className="w-100">
+            Login
+          </Button>
+        </Form>
+        <div className="my-4">
+          <button className="mx-4 border-0 px-2 py-2 mt-3 bg-primary text-white rounded">
+            Login With Google
+          </button>
+          <button className="mx-4 border-0 px-2 py-2 mt-3 bg-primary text-white rounded">
+            Login With Github
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
